773376e536def3cead39c94e543f2ea284ce4c94
Giosuè Lentini <63297733+Giosue1994@users.noreply.github.com>
2024-04-23 19:15:52 +0200

    Add place screen & navigation

--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -1,10 +1,9 @@
 import { StyleSheet, Text, View } from "react-native";
-import PlaceForm from "../components/Places/PlaceForm";
 
-export default function AddPlace() {
+export default function PlaceForm() {
   return (
     <View>
-      <PlaceForm />
+      <Text>PlaceForm</Text>
     </View>
   );
 }
--- screens/AllPlaces.js
+++ screens/AllPlaces.js
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View } from "react-native";
-import PlacesList from "../components/PlacesList";
+import PlacesList from "../components/Places/PlacesList";
 
 export default function AllPlaces() {
   return <PlacesList places={[]} />;
