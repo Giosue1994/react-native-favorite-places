a6b52bbb7a4355e30e85ad8476d12555b93f1ff3
Giosuè Lentini <63297733+Giosue1994@users.noreply.github.com>
2024-04-28 17:00:22 +0200

    fetch places from database

--- screens/AddPlace.js
+++ screens/AddPlace.js
@@ -1,12 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
 import PlaceForm from "../components/Places/PlaceForm";
+import { insertPlace } from "../util/database";
 
 export default function AddPlace({ navigation }) {
-  function createPlaceHandler(place) {
-    console.log(place);
-    navigation.navigate("AllPlaces", {
-      place: place,
-    });
+  async function createPlaceHandler(place) {
+    await insertPlace(place);
+
+    // navigation.navigate("AllPlaces", {
+    //   place: place,
+    // });
   }
 
   return <PlaceForm onCreatePlace={createPlaceHandler} />;
